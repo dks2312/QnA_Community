3844107c1d4d6bb256e2294b262498e21c235795
DKS2312 <dtg2312@gmail.com>
2022-07-05 18:07:21 +0900

    DB연결 + 로그인 기능 구현

---
+++ QnACommunity/src/main/java/Test/Login.java
@@ -0,0 +1,33 @@
+package Test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DB.MemberDAO;
+import DB.MemberVo;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		MemberDAO userDAO = new MemberDAO();
+		MemberVo userVo = new MemberVo(id, pw);
+		boolean isMember = userDAO.list(userVo);
+		
+		if(isMember) response.sendRedirect("index.html");
+		else response.sendRedirect("login_page.html");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
